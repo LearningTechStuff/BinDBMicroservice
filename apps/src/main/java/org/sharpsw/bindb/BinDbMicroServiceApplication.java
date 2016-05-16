abb2223691034f7221d862e7438d0acd1aa7974c
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2016-05-15 21:45:25 -0300

    More code

--- apps/src/main/java/org/sharpsw/bindb/BinDbMicroServiceApplication.java
+++ apps/src/main/java/org/sharpsw/bindb/BinDbMicroServiceApplication.java
@@ -1,8 +1,18 @@
 package org.sharpsw.bindb;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@Configuration
+@EnableConfigurationProperties
+@EnableAutoConfiguration
+@ComponentScan
+@EnableWebMvc
 @SpringBootApplication
 public class BinDbMicroServiceApplication {
 
