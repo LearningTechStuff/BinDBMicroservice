35ac651aeffb9781fa7d5497bac8f1461c7f1534
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2016-05-15 21:22:44 -0300

    Initial commit

---
+++ apps/src/main/java/org/sharpsw/bindb/BinDbMicroServiceApplication.java
@@ -0,0 +1,12 @@
+package org.sharpsw.bindb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinDbMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinDbMicroServiceApplication.class, args);
+	}
+}
